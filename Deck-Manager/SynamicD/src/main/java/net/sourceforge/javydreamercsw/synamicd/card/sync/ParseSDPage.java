5c091a72ec495b59c69495deb125a6dc4a41217a
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2013-10-23 17:38:32 -0500

    Initial Commit of SynamicD module.

---
+++ Deck-Manager/SynamicD/src/main/java/net/sourceforge/javydreamercsw/synamicd/card/sync/ParseSDPage.java
@@ -0,0 +1,9 @@
+package net.sourceforge.javydreamercsw.synamicd.card.sync;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
+ */
+public class ParseSDPage {
+
+}
---
+++ Deck-Manager/SynamicD/src/main/java/net/sourceforge/javydreamercsw/synamicd/card/sync/ParseSDSets.java
@@ -0,0 +1,15 @@
+package net.sourceforge.javydreamercsw.synamicd.card.sync;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
+ */
+public class ParseSDSets extends ParseSDPage {
+
+    public void load() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
